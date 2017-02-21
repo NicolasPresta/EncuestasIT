8d2a5db4fbd2f9fb7f1172be0791f221f44d39f4
nicolaspresta <presta_26@hotmail.com>
2017-02-21 17:39:43 -0300

    avances

--- scripts/main.R
+++ scripts/main.R
@@ -1,7 +1,15 @@
-# Definimos algunas constantes que nos seràn utiles durante el proceso.
+# ----------------------  Importamos funciones  ----------------------  
+source("./funciones/quitarSaltosLineaComentarios.R")
+source("./funciones/cantidadOcurrenciasStr.R")
+
+
+# ----------------------  Constantes  ----------------------  
+
 FILE_ENCUESTAS  <- "./datos/encuestas.txt"
 FILE_TABLAS  <- "./datos/tablas_anexas.txt"
 
+
+# ----------------------  Descarga  ----------------------
 
 # Descargamos los datos en crudo del sitio de encuestasIT
 if(!file.exists(FILE_ENCUESTAS))
@@ -20,6 +28,7 @@ if(!file.exists(FILE_TABLAS))
 # al levantar el archivo directamente como un .csv, por lo que necesitamos "sanitizar" este campo antes
 # de proseguir
 
+# ----------------------  Preprocesado  ----------------------
 
 # Levantamos el archico como un documento de texto
 txtdata <- readLines(FILE_ENCUESTAS) 
@@ -74,11 +83,117 @@ length(txtdata)
 write(renglonesInconsistentes, "./datos/encuestas_unclear.txt")
 write(renglonesConsistentes, "./datos/encuestas_clear.txt")
 
-# Falta resolver el tema de todos los registros rotos
+# TODO: Falta resolver el tema de todos los registros rotos
+
+# ----------------------  Levantar DataFrame  ----------------------
 
 # Levantamos el archivo ya formateado, 
 encuestas <- read.csv("./datos/encuestas_clear.txt")
 
+# ----------------------  Procesado de caracteristicas  ----------------------
+
+# Pasamos la fecha al formato correcto
+encuestas$Fecha1 <- 
+encuestas$Fecha <- lapply(encuestas$Fecha, as.character)
+encuestas$Fecha <- lapply(encuestas$Fecha, function(x){substr(x, 0, 16)})
+encuestas$Fecha <- lapply(encuestas$Fecha, function(x){strptime(x, "%Y-%m-%d %H:%M")})
+
+# Buscamos valores faltantes
+na_count <-sapply(encuestas, function(y) sum(length(which(is.na(y)))))
+na_count <- data.frame(na_count)
+
+# Quitamos las columnas que tienen todos NA
+encuestas$SalarioActualBruto <- NULL
+encuestas$IdArea <- NULL
+encuestas$puestoGenerico <- NULL
+encuestas$especialidadGenerico <- NULL
+
+# La mayoria de los datos de la columna NivelRemunerativo son NA, y sumado a que tampoco hay
+# referencias sobre ese campo, lo mejor parece ser que es quitarla
+# veamos si existe alguna correlaciòn entre el nivelRemunerativo y el salario
+boxplot(SalarioActualNeto ~ NivelRemunerativo, data = encuestas)
+
+# No se ve ninguna relación clara, quitamos entonces nivelRemunerativo
+encuestas$NivelRemunerativo <- NULL
+
+# Vemos que hay un registro con IdPais en NA, lo eliminamos
+encuestas <- encuestas[complete.cases(encuestas[,"IdPais"]),]
+
+# Nos quedamos solo con los de Argentina, ya que para este estudio no nos interesa el resto. 
+# Ademas sus salarios están expresados en moneda local de cada país, lo que requeriria conversion
+encuestas <- subset(encuestas, IdPais == 1)
+
+
+# Esto último tambien eliminó el unico registro que tenia un NA en NivelDeDesconfianza, genial!
+
+
+
+# TODO: Falta levantar las tablas auxiliares y setear bien los factors
+
+# ---------------------- Creación de caracteristicas  ---------------------- 
+encuestas$DiferenciaSalarioRealIdeal <- encuestas$SalarioIdealNeto - encuestas$SalarioActualNeto
+
+# ---------------------- Limpieza de valores anomalos  ---------------------- 
+
+# todas las (edad > 65) las vamos a considerar anomalas y vamos a desechar esos registros
+encuestas <- encuestas[encuestas$Edad < 66, ]
+hist(encuestas$Edad)
+rug(encuestas$Edad)
+
+# todas las (horas trabajadas >= 150) las vamos a considerar anomalas y vamos a desechar esos registros
+encuestas <- encuestas[encuestas$horasTrabajadasXSemana < 150, ]
+hist(encuestas$horasTrabajadasXSemana)
+rug(encuestas$horasTrabajadasXSemana)
+
+# todos los (meses en el puesto actual >= 360) los vamos a considerar anomalas y vamos a desechar esos registros
+encuestas <- encuestas[encuestas$MesesEnElPuestoActual < 480, ]
+hist(encuestas$MesesEnElPuestoActual)
+rug(encuestas$MesesEnElPuestoActual)
+
+# todos los (salario actual neto >= 150000) los vamos a considerar anomalas y vamos a desechar esos registros
+encuestas <- encuestas[encuestas$SalarioActualNeto < 150000, ]
+hist(encuestas$SalarioActualNeto)
+rug(encuestas$SalarioActualNeto)
+
+# todos los (salario ideal neto >= 150000) los vamos a considerar anomalas y vamos a desechar esos registros
+encuestas <- encuestas[encuestas$SalarioIdealNeto < 150000, ]
+hist(encuestas$SalarioIdealNeto)
+rug(encuestas$SalarioIdealNeto)
+
+
+# ---------------------- Exploración grafica ----------------------
+
+
+# Las variables mas interesantes que nos interesan explorar, en principio, son:
+# SalarioActualNeto
+# IdNivelEducativo
+# IdSexo
+# horasTrabajadasXSemana
+
+# histogramas de salarios netos para hombres y mujeres en los puestos bajos
+par(mfrow = c(2, 1), mar = c(4, 4, 2, 1))
+hist(subset(encuestas, IdSexo == 1 & SalarioActualNeto < 20000)$SalarioActualNeto)
+hist(subset(encuestas, IdSexo == 2 & SalarioActualNeto < 20000)$SalarioActualNeto)
+
+par(mfrow = c(1, 1), mar = c(4, 4, 1, 1))
+
+# Salario y nivel educativo
+boxplot(SalarioActualNeto ~ IdNivelEducativo, data = subset(encuestas, SalarioActualNeto < 50000 & ))
+# La media sube mientras sube el nivel educativo.
+# Ahora veamos la relación entre nivel educativo y horas de trabajo por semana
+
+plot(encuestas$IdNivelEducativo, encuestas$horasTrabajadasXSemana)
+# Aquellos que tienen universitario incompleto tienen una mayor media de cantidad de horas que el resto.
+# Parece lógico.
+
+# Veamos la relación entre el salario y la antiguedad, para puestos bajos y medios
+with(subset(encuestas, SalarioActualNeto < 40000), plot(SalarioActualNeto, MesesEnElPuestoActual) )
+
+# Veamos la relación entre el salario y la edad, para puestos bajos y medios
+with(subset(encuestas, SalarioActualNeto < 40000), plot(SalarioActualNeto, Edad) )
+# Hay una correlación, aunque se ameceta luego de los 40
+
+# veamos el salario medio para cada nivel educativo
 
 
 
