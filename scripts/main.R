1cccae03204648e6e052ea8f3b9690ee968f7c4b
nicolaspresta <presta_26@hotmail.com>
2017-02-21 21:17:34 -0300

    avance en exploración grafica y armado de caracteristicas

--- scripts/main.R
+++ scripts/main.R
@@ -1,4 +1,5 @@
 # ----------------------  Importamos funciones  ----------------------  
+
 source("./funciones/quitarSaltosLineaComentarios.R")
 source("./funciones/cantidadOcurrenciasStr.R")
 
@@ -28,7 +29,7 @@ if(!file.exists(FILE_TABLAS))
 # al levantar el archivo directamente como un .csv, por lo que necesitamos "sanitizar" este campo antes
 # de proseguir
 
-# ----------------------  Preprocesado  ----------------------
+# ----------------------  Preprocesado archivo texto  ----------------------
 
 # Levantamos el archico como un documento de texto
 txtdata <- readLines(FILE_ENCUESTAS) 
@@ -83,26 +84,24 @@ length(txtdata)
 write(renglonesInconsistentes, "./datos/encuestas_unclear.txt")
 write(renglonesConsistentes, "./datos/encuestas_clear.txt")
 
-# TODO: Falta resolver el tema de todos los registros rotos
+# TODO: Falta resolver el tema de todos los registros rotos por las comas.
+# Quedo a la espera de que Ignacio publique el nuevo dataset con otro delimitador 
+# Mientras continuo analisís con los datos que están OK
 
 # ----------------------  Levantar DataFrame  ----------------------
 
 # Levantamos el archivo ya formateado, 
 encuestas <- read.csv("./datos/encuestas_clear.txt")
 
-# ----------------------  Procesado de caracteristicas  ----------------------
+# ----------------------  Tratamiento Valores Faltantes  ----------------------
 
-# Pasamos la fecha al formato correcto
-encuestas$Fecha1 <- 
-encuestas$Fecha <- lapply(encuestas$Fecha, as.character)
-encuestas$Fecha <- lapply(encuestas$Fecha, function(x){substr(x, 0, 16)})
-encuestas$Fecha <- lapply(encuestas$Fecha, function(x){strptime(x, "%Y-%m-%d %H:%M")})
 
-# Buscamos valores faltantes
+# Analisamos valores faltantes
 na_count <-sapply(encuestas, function(y) sum(length(which(is.na(y)))))
 na_count <- data.frame(na_count)
+plot(na_count$na_count)
 
-# Quitamos las columnas que tienen todos NA
+# Quitamos las columnas que tienen todos NA 
 encuestas$SalarioActualBruto <- NULL
 encuestas$IdArea <- NULL
 encuestas$puestoGenerico <- NULL
@@ -110,7 +109,7 @@ encuestas$especialidadGenerico <- NULL
 
 # La mayoria de los datos de la columna NivelRemunerativo son NA, y sumado a que tampoco hay
 # referencias sobre ese campo, lo mejor parece ser que es quitarla
-# veamos si existe alguna correlaciòn entre el nivelRemunerativo y el salario
+# antes veamos si existe alguna correlaciòn entre el nivelRemunerativo y el salario
 boxplot(SalarioActualNeto ~ NivelRemunerativo, data = encuestas)
 
 # No se ve ninguna relación clara, quitamos entonces nivelRemunerativo
@@ -120,18 +119,124 @@ encuestas$NivelRemunerativo <- NULL
 encuestas <- encuestas[complete.cases(encuestas[,"IdPais"]),]
 
 # Nos quedamos solo con los de Argentina, ya que para este estudio no nos interesa el resto. 
-# Ademas sus salarios están expresados en moneda local de cada país, lo que requeriria conversion
+# Ademas sus salarios están expresados en moneda local de cada país, lo que requeriria una conversión
+# Por el momento el estudio se basará en los datos de Argentina
 encuestas <- subset(encuestas, IdPais == 1)
+encuestas$IdPais <- NULL
 
 
 # Esto último tambien eliminó el unico registro que tenia un NA en NivelDeDesconfianza, genial!
 
 
+# Analisamos valores faltantes nuevamente
+na_count <-sapply(encuestas, function(y) sum(length(which(is.na(y)))))
+na_count <- data.frame(na_count)
+plot(na_count$na_count)
 
-# TODO: Falta levantar las tablas auxiliares y setear bien los factors
+# Parece estar suficientemente limpio el dataset de valores vacios.
+
+
+# ----------------------  Ajuste de tipos de datos  ----------------------
+
+# Pasamos la fecha al formato correcto
+encuestas$Fecha <- as.character(encuestas$Fecha)
+encuestas$Fecha <- as.POSIXlt(encuestas$Fecha)
+
+
+# Levantamos las tablas auxiliares
+tabla_sexo <- read.table(FILE_TABLAS, header = TRUE, sep=",", nrows =3, comment.char = "-")
+tabla_nivel_educativo <- read.table(FILE_TABLAS, header = TRUE, sep="\t", nrows = 10, skip=7, comment.char = "-")
+tabla_tipo_empresa <- read.table(FILE_TABLAS, header = TRUE, sep="\t", nrows = 7, skip=20, comment.char = "-")
+tabla_provincia <- read.table(FILE_TABLAS, header = TRUE, sep="\t", nrows = 413, skip=30)
+tabla_puesto <- read.table(FILE_TABLAS, header = TRUE, sep="\t", nrows = 95, skip=446)
+tabla_tecnologia <- read.table(FILE_TABLAS, header = TRUE, sep="\t", nrows = 72, skip=544)
+
+# Quitamos las provincias que no sean de argentina
+tabla_provincia <- tabla_provincia[tabla_provincia$IdPais == 1, ]
+
+# Seteamos los factors
+table(encuestas$IdSexo)
+encuestas$IdSexo <- factor(encuestas$IdSexo, levels = tabla_sexo$IdSexo, labels = tabla_sexo$Nombre)
+table(encuestas$IdSexo)
+
+table(encuestas$IdNivelEducativo)
+encuestas$IdNivelEducativo <- factor(encuestas$IdNivelEducativo, levels = tabla_nivel_educativo$IdNivelEducativo, labels = tabla_nivel_educativo$Nombre)
+table(encuestas$IdNivelEducativo)
+
+table(encuestas$IdTipoDeEmpresa)
+encuestas$IdTipoDeEmpresa <- factor(encuestas$IdTipoDeEmpresa, levels = tabla_tipo_empresa$IdTipoDeEmpresa, labels = tabla_tipo_empresa$Nombre)
+table(encuestas$IdTipoDeEmpresa)
+
+table(encuestas$IdProvincia)
+encuestas$IdProvincia <- factor(encuestas$IdProvincia, levels = tabla_provincia$IdProvincia, labels = tabla_provincia$Nombre)
+table(encuestas$IdProvincia)
+
+table(encuestas$IdPuesto)
+encuestas$IdPuesto <- factor(encuestas$IdPuesto, levels = tabla_puesto$IdPuesto, labels = tabla_puesto$Nombre)
+table(encuestas$IdPuesto)
+
+table(encuestas$IdTecnologiaPrincipal)
+encuestas$IdTecnologiaPrincipal <- factor(encuestas$IdTecnologiaPrincipal, levels = tabla_tecnologia$IdTecnologiaPrincipal, labels = tabla_tecnologia$Nombre)
+table(encuestas$IdTecnologiaPrincipal)
+
+table(encuestas$TrabajaDesdeCasa)
+encuestas$TrabajaDesdeCasa <- factor(encuestas$TrabajaDesdeCasa, levels = c("True", "False"), labels = c("Si", "No"))
+table(encuestas$TrabajaDesdeCasa)
+
+table(encuestas$LeGustaTrabajarDesdeCasa)
+encuestas$LeGustaTrabajarDesdeCasa <- factor(encuestas$LeGustaTrabajarDesdeCasa, levels = c("True", "False"), labels = c("Si", "No"))
+table(encuestas$LeGustaTrabajarDesdeCasa)
+
+table(encuestas$CambioPorMejorSalario)
+encuestas$CambioPorMejorSalario <- factor(encuestas$CambioPorMejorSalario, levels = c("True", "False"), labels = c("Si", "No"))
+table(encuestas$CambioPorMejorSalario)
+
+table(encuestas$CambioPorMejorAmbiente)
+encuestas$CambioPorMejorAmbiente <- factor(encuestas$CambioPorMejorAmbiente, levels = c("True", "False"), labels = c("Si", "No"))
+table(encuestas$CambioPorMejorAmbiente)
+
+table(encuestas$CambioPorFormaDeTrabajo)
+encuestas$CambioPorFormaDeTrabajo <- factor(encuestas$CambioPorFormaDeTrabajo, levels = c("True", "False"), labels = c("Si", "No"))
+table(encuestas$CambioPorFormaDeTrabajo)
+
+table(encuestas$CambioPorTecnologia)
+encuestas$CambioPorTecnologia <- factor(encuestas$CambioPorTecnologia, levels = c("True", "False"), labels = c("Si", "No"))
+table(encuestas$CambioPorTecnologia)
+
+table(encuestas$CambioPorCercania)
+encuestas$CambioPorCercania <- factor(encuestas$CambioPorCercania, levels = c("", "True", "False"), labels = c("No informa", "Si", "No"))
+table(encuestas$CambioPorCercania)
+
+table(encuestas$CambioPorMenorCargaHoraria)
+encuestas$CambioPorMenorCargaHoraria <- factor(encuestas$CambioPorMenorCargaHoraria, levels = c("", "True", "False"), labels = c("No informa", "Si", "No"))
+table(encuestas$CambioPorMenorCargaHoraria)
+
+table(encuestas$CambioPorOportunidadDeCarrera)
+encuestas$CambioPorOportunidadDeCarrera <- factor(encuestas$CambioPorOportunidadDeCarrera, levels = c("", "True", "False"), labels = c("No informa", "Si", "No"))
+table(encuestas$CambioPorOportunidadDeCarrera)
+
+table(encuestas$TienePersonasACargo)
+encuestas$TienePersonasACargo <- factor(encuestas$TienePersonasACargo, levels = c("", "True", "False"), labels = c("No informa", "Si", "No"))
+table(encuestas$TienePersonasACargo)
+
+
 
 # ---------------------- Creación de caracteristicas  ---------------------- 
+
 encuestas$DiferenciaSalarioRealIdeal <- encuestas$SalarioIdealNeto - encuestas$SalarioActualNeto
+
+
+encuestas$SalarioNetoPorHora <- encuestas$SalarioActualNeto / encuestas$horasTrabajadasXSemana
+
+# Carga laboral: En relación a las horas que dedica al trabajo
+encuestas$CargaLaboral <- cut(encuestas$horasTrabajadasXSemana, 5, labels = c("Part Time", "Full Time", "Extra Time", "Very Extra Time", "Extreme Time"))
+
+# Antiguedad: Nivel de antiguedad en el actual trabajo
+encuestas$Antiguedad <- cut(encuestas$MesesEnElPuestoActual,quantile(encuestas$MesesEnElPuestoActual,(0:4)/4), labels = c("Junior", "SemiSenior", "Senior", "Expert"))
+
+# Experiencia: Nivel de antiguedad en cualquier trabajo
+encuestas$Experiencia <- cut(encuestas$MesesDeExperiencia,quantile(encuestas$MesesDeExperiencia,(0:4)/4), labels = c("Junior", "SemiSenior", "Senior", "Expert"))
+
 
 # ---------------------- Limpieza de valores anomalos  ---------------------- 
 
@@ -140,8 +245,9 @@ encuestas <- encuestas[encuestas$Edad < 66, ]
 hist(encuestas$Edad)
 rug(encuestas$Edad)
 
-# todas las (horas trabajadas >= 150) las vamos a considerar anomalas y vamos a desechar esos registros
-encuestas <- encuestas[encuestas$horasTrabajadasXSemana < 150, ]
+# todas las (horas trabajadas >= 120 o <= 18) las vamos a considerar anomalas y vamos a desechar esos registros
+encuestas <- encuestas[encuestas$horasTrabajadasXSemana < 120, ]
+encuestas <- encuestas[encuestas$horasTrabajadasXSemana > 18, ]
 hist(encuestas$horasTrabajadasXSemana)
 rug(encuestas$horasTrabajadasXSemana)
 
@@ -160,31 +266,72 @@ encuestas <- encuestas[encuestas$SalarioIdealNeto < 150000, ]
 hist(encuestas$SalarioIdealNeto)
 rug(encuestas$SalarioIdealNeto)
 
+# Todas las diferencias de salario real e ideal mayores a 25000 y menores a -5000 las eliminamos
+# Por considerarlas anomalas.
+encuestas <- encuestas[encuestas$DiferenciaSalarioRealIdeal < 25000, ]
+encuestas <- encuestas[encuestas$DiferenciaSalarioRealIdeal > -5000, ]
+hist(encuestas$DiferenciaSalarioRealIdeal)
+rug(encuestas$DiferenciaSalarioRealIdeal)
+
+# Todos los salarios netos por hora mayores a 1500 los vamos a eliminar por considerarlos anomalos
+encuestas <- encuestas[encuestas$SalarioNetoPorHora < 1500, ]
+hist(encuestas$SalarioNetoPorHora)
+rug(encuestas$SalarioNetoPorHora)
+
+
+# Todos los "Extreme Time" los vamos a borrar por no ser representativos
+encuestas <- encuestas[encuestas$CargaLaboral != "Extreme Time", ]
+table(encuestas$CargaLaboral)
+
+
+# Borramos los levels que no se usan
+encuestas <- droplevels(encuestas)
 
 # ---------------------- Exploración grafica ----------------------
 
+# distribución de sueldo netos por hora en función del puesto
+par(mar=c(1,18,1,1))
+bymedian <- with(encuestas, reorder(IdPuesto, -SalarioNetoPorHora, median))
+boxplot(SalarioNetoPorHora ~ bymedian, encuestas, horizontal = TRUE, las = 2, col="green")
 
-# Las variables mas interesantes que nos interesan explorar, en principio, son:
-# SalarioActualNeto
-# IdNivelEducativo
-# IdSexo
-# horasTrabajadasXSemana
+# distribución de sueldo netos por hora en función del nivel educativo
+par(mar=c(1,15,1,1))
+bymedian <- with(encuestas, reorder(IdNivelEducativo, -SalarioNetoPorHora, median))
+boxplot(SalarioNetoPorHora ~ bymedian, encuestas, horizontal = TRUE, las = 2, col="green")
 
-# histogramas de salarios netos para hombres y mujeres en los puestos bajos
-par(mfrow = c(2, 1), mar = c(4, 4, 2, 1))
-hist(subset(encuestas, IdSexo == 1 & SalarioActualNeto < 20000)$SalarioActualNeto)
-hist(subset(encuestas, IdSexo == 2 & SalarioActualNeto < 20000)$SalarioActualNeto)
+# distribución de sueldo netos por hora en función del sexo
+par(mar=c(1,10,1,1))
+bymedian <- with(encuestas, reorder(IdSexo, -SalarioNetoPorHora, median))
+boxplot(SalarioNetoPorHora ~ bymedian, encuestas, horizontal = TRUE, las = 2, col="green")
 
-par(mfrow = c(1, 1), mar = c(4, 4, 1, 1))
+# distribución de sueldo netos por hora en función del sexo
+par(mar=c(1,10,1,1))
+bymedian <- with(encuestas, reorder(IdSexo, -SalarioNetoPorHora, median))
+boxplot(SalarioNetoPorHora ~ bymedian, encuestas, horizontal = TRUE, las = 2, col="green")
 
-# Salario y nivel educativo
-boxplot(SalarioActualNeto ~ IdNivelEducativo, data = subset(encuestas, SalarioActualNeto < 50000 & ))
-# La media sube mientras sube el nivel educativo.
-# Ahora veamos la relación entre nivel educativo y horas de trabajo por semana
+# distribución de sueldo netos por hora en función de las horas trabajadas por semana
+par(mar=c(1,8,1,1))
+bymedian <- with(encuestas, reorder(CargaLaboral, -SalarioNetoPorHora, median))
+boxplot(SalarioNetoPorHora ~ bymedian, encuestas, horizontal = TRUE, las = 2, col="green")
 
-plot(encuestas$IdNivelEducativo, encuestas$horasTrabajadasXSemana)
-# Aquellos que tienen universitario incompleto tienen una mayor media de cantidad de horas que el resto.
-# Parece lógico.
+# distribución de sueldo netos por hora en función de la tecnologia
+par(mar=c(1,7,1,1))
+bymedian <- with(encuestas, reorder(Experiencia, -SalarioNetoPorHora, median))
+boxplot(SalarioNetoPorHora ~ bymedian, encuestas, horizontal = TRUE, las = 2, col="green")
+
+
+# distribución de sueldo netos por hora en función de la tecnologia
+par(mar=c(1,7,1,1))
+bymedian <- with(encuestas, reorder(Antiguedad, -SalarioNetoPorHora, median))
+boxplot(SalarioNetoPorHora ~ bymedian, encuestas, horizontal = TRUE, las = 2, col="green")
+
+
+# distribución de sueldo netos por hora en función de la tecnologia
+par(mar=c(1,7,1,1))
+bymedian <- with(encuestas, reorder(IdTecnologiaPrincipal, -SalarioNetoPorHora, median))
+boxplot(SalarioNetoPorHora ~ bymedian, encuestas, horizontal = TRUE, las = 2, col="green")
+
+
 
 # Veamos la relación entre el salario y la antiguedad, para puestos bajos y medios
 with(subset(encuestas, SalarioActualNeto < 40000), plot(SalarioActualNeto, MesesEnElPuestoActual) )
