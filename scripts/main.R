630411dec2047adcf7a088e5da26a5732d126181
nicolaspresta <presta_26@hotmail.com>
2017-02-23 15:56:40 -0300

    agrego graficos

--- scripts/main.R
+++ scripts/main.R
@@ -359,12 +359,14 @@ with(subset(encuestas, SalarioActualNeto < 40000), plot(SalarioActualNeto, Meses
 
 
 # Veamos la relación entre el salario y la edad, para el año 2013, discriminado por sexo
+par(mar=c(5,5,1,1))
 with(subset(encuestas, Fecha$year == 113 & Edad > 1 & IdSexo == "Masculino"), plot(SalarioNetoPorHora, Edad))
 with(subset(encuestas, Fecha$year == 113 & Edad > 1 & IdSexo == "Femenino"), points(SalarioNetoPorHora, Edad, col="red"))
 model <- lm(Edad ~ SalarioNetoPorHora, subset(encuestas, Fecha$year == 113 & Edad > 1 & IdSexo == "Masculino"))
 abline(model, lwd = 2)
 model <- lm(Edad ~ SalarioNetoPorHora, subset(encuestas, Fecha$year == 113 & Edad > 1 & IdSexo == "Femenino"))
 abline(model, lwd = 2, col="red")
+legend("topright", legend = c("Masculino", "Femenino"), pch = 1, col = c("black", "red"))
 
 
 
